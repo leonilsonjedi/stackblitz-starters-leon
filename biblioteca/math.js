d51bbd8739d3f6adff55cb156f39385d276db664
leonilsonjedi <leonilsonjedi@gmail.com>
2026-05-29 01:06:40 -0300

    feito

---
+++ biblioteca/math.js
@@ -0,0 +1,19 @@
+function soma(a, b) {
+    let soma = a + b; 
+    return soma;
+  }
+
+  const subtrair = function(a, b) { 
+    return a - b; 
+  }
+
+  const multiplicar = function(a, b) { 
+    return a * b; 
+  }
+
+  const dividir = function(a, b) { 
+    return a / b; 
+  }
+
+  module.exports = { soma, subtrair, multiplicar, dividir }
+  
---
+++ usamath.js
@@ -0,0 +1,10 @@
+const oper = require('./biblioteca/math.js');
+
+var a = 55, b = 5;
+
+console.log(oper.soma(a, b)); 
+console.log(oper.subtrair(a, b));
+console.log(oper.multiplicar(a, b));
+console.log(oper.dividir(a, b));
+
+
